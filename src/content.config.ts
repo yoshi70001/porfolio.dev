487ee7ccda6560091cbd8c7f2b918e4e8780f0e6
JorgeEspinoza976 <jorge.espinoza@2win.cl>
2026-03-18 18:41:34 -0500

    refactor(config): renombra archivo de configuración y añade loader a colección blog

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,9 @@
 
-import { defineCollection, z } from "astro:content";
-
-const blogCollection = defineCollection({
+import { defineCollection } from "astro:content";
+import { z } from 'astro/zod'
+import { glob } from "astro/loaders";
+const blog = defineCollection({
+  loader: glob({ pattern: '**/*.md', base: './src/content/blog' }),
   schema: z.object({
     title: z.string(),
     pubDate: z.date(),
@@ -15,6 +17,4 @@ const blogCollection = defineCollection({
   }),
 });
 
-export const collections = {
-  'blog': blogCollection,
-};
+export const collections = { blog };
